fc3d6a9682760233bbdf30216d2e429ee420974c
Jerome Toulouse <toulouse.jerome@gmail.com>
2020-10-01 18:08:27 +0200

    bd ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,34 @@
 Rails.application.routes.draw do
-  resources :contacts
-  resources :reviews
-  resources :restaurants
+
   devise_for :users
+  get 'users/first_nam'
+  get 'users/last_name'
+  get 'users/email'
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
+  # get "restaurants" => "restaurants#index"
+  # get "restaurants/:id" => "restaurants#show"
+  # get "restaurants/:new" => "restaurants#new"
+  # post "restaurants" => "restaurants#create"
+  # get "restaurants/:id/edit" => "restaurants#edit"
+  # patch "restaurants/:id" => "restaurants#update"
+  # delete "restaurants/:id" => "restaurants#restroy"
+
+  resources :restaurants do
+
+    resources :reviews, only: [:new, :create]
+
+    collection do
+      get :top
+    end
+
+    member do
+      # get :chef # /restaurant/:id/chef par exemple
+    end
+  end
+  resources :reviews, only: [:destroy]
+  resources :contacts, only: [:new, :create]
 end
