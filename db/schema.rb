07a016172c39c117e1c7c2f04b7df0b4eebd89cc
Jérôme toulouse <toulouse.jerome@gmail.com>
2020-10-23 20:16:25 +0200

    show ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_19_171056) do
+ActiveRecord::Schema.define(version: 2020_10_21_172501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2020_10_19_171056) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
+    t.text "descriptionDeux"
     t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
