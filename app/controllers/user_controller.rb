b24f816960475e2d5a389c580b186f28e6e3d4fc
Jérôme toulouse <toulouse.jerome@gmail.com>
2020-10-25 20:55:31 +0100

    ok

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,31 @@
 class UserController < ApplicationController
 
+before_action :authenticate_user!
 
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to :back, :alert => "Access denied."
+    end
+  end
+
+  def update
+    @user.update(user_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :gender, :password, :photos)
+  end
+
+  def set_restaurant
+    @user = User.find(params[:id])
+  end
 
 end
